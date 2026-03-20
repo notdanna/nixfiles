64f6877e98f26d6961ec567947690e8f9dae0cb3
notdanna <afrancor1900@alumno.ipn.mx>
2026-03-20 16:14:01 -0600

    fix: useless comment

--- home.nix
+++ home.nix
@@ -2,10 +2,9 @@
   home.username = "dam";
   home.homeDirectory = if pkgs.stdenv.isDarwin then "/Users/dam" else "/home/dam";
   
-  home.stateVersion = "24.11"; #
+  home.stateVersion = "24.11";
 
   imports = [
-    # Asegúrate de que estas rutas apunten a donde moviste tus archivos comunes
     ./modules/common/zsh.nix
     ./modules/common/git.nix
     ./modules/common/packages.nix
